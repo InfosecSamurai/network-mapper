8f7da0751b7074c014a0e9448d305d56cbe02443
InfosecSamurai <William.B.Shaw@proton.me>
2025-03-29 17:32:04 +0000

    setupTests.js

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,1 +1,22 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/extend-expect';
 
+// Mock window.matchMedia which is not implemented in JSDOM
+window.matchMedia = window.matchMedia || function() {
+  return {
+    matches: false,
+    addListener: function() {},
+    removeListener: function() {}
+  };
+};
+
+// Mock ResizeObserver which is not implemented in JSDOM
+class ResizeObserver {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+window.ResizeObserver = ResizeObserver;
