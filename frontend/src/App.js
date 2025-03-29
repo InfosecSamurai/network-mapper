661cba5daded80f4e104854cb8811bd74bcfe0e1
InfosecSamurai <William.B.Shaw@proton.me>
2025-03-29 17:30:54 +0000

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import { NetworkProvider } from './context/NetworkContext';
+import NetworkCanvas from './components/NetworkCanvas';
+import Toolbar from './components/Toolbar';
+import DeviceDetails from './components/DeviceDetails';
+import './App.css';
 
+function App() {
+  return (
+    <NetworkProvider>
+      <div className="App">
+        <Toolbar />
+        <div className="main-content">
+          <NetworkCanvas />
+          <DeviceDetails />
+        </div>
+      </div>
+    </NetworkProvider>
+  );
+}
+
+export default App;
